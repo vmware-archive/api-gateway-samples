a18e0708912f9346c645587792bec76fb7d19129
xtreme-jenkins <jenkins@xtremelabs.com>
2014-07-03 13:30:31 -0400

    updating to latest release

--- sample-hello/src/main/resources/app.js
+++ sample-hello/src/main/resources/app.js
@@ -16,7 +16,7 @@ appRouter.all('/*catchall', function(req,res) {
     links :[
     {rel: 'Hello world', href: baseUrl+'/hello'},
     {rel: 'Echos the message back. Also an example of module definition and loading.', href: baseUrl+'/echo/:message'},
-    {rel: 'Interactive test runner. You can modify tests and applicaiton code at runtime', href: "http://localhost:8080/tests.html"}
+    {rel: 'Interactive test runner. You can modify tests and applicaiton code at runtime', href: "http://localhost:8080/test/index.html"}
   ]});
 });
 
