2efd138f9fcb773263566137d3a110cd69d92450
xtreme-jenkins <jenkins@xtremelabs.com>
2014-07-16 17:13:07 -0400

    updating to latest release

--- sample-hello/src/test/java/com/example/hello/ApplicationSpecTest.java
+++ sample-hello/src/test/java/com/example/hello/ApplicationSpecTest.java
@@ -1,11 +1,20 @@
 package com.example.hello;
 
+import java.io.IOException;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
 
 public class ApplicationSpecTest extends com.pivotal.mss.apigateway.test.ApplicationSpecTest {
 
 	public ApplicationSpecTest(String specName, String specFile) {
         super(specName, specFile);
+    }
+	
+	@Parameters(name = "{0}")
+    public static Collection<Object[]> getParameters() throws IOException {
+        return com.pivotal.mss.apigateway.test.ApplicationSpecTest.getParameters("spec");
     }
 
     @Test
