dc89e663f46e95d1e85b543e13094606f99e65d9
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2014-09-25 08:14:41 -0400

    Final application

--- sample-ratelimit/src/main/java/io/pivotal/Application.java
+++ sample-ratelimit/src/main/java/io/pivotal/Application.java
@@ -1,5 +1,8 @@
 package io.pivotal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +19,7 @@ public class Application extends com.pivotal.mss.apigateway.Application {
 	
 	public static void main(String[] args) {
 		runApplication(Application.class, args);
+		List<Integer> names = new ArrayList<Integer>();
 	}
 	
 	@Bean
