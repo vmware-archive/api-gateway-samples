8d39d08966b8fce868c44dfdccee8f3ea7edb8e5
Topher Bullock <cbullock@pivotal.io>
2015-05-27 15:19:01 -0400

    [#95438836] Add more Readme!
    https://www.pivotaltracker.com/story/show/95438836

--- sample-ratelimit/src/main/resources/app.js
+++ sample-ratelimit/src/main/resources/app.js
@@ -1,7 +1,10 @@
+var Rate = Packages.io.pivotal.api.rate.Rate;
+var rateLimiter = spring.getBean("rateLimiter");
+
 var Router = require("Router");
 var RatedRouter = require("modules/RatedRouter")
-var _router = new Router();
-var appRouter = new RatedRouter(_router);
+var router = new Router();
+var appRouter = new RatedRouter(router);
 
 var fbGraphClient = require('http')({
 	baseUrl : 'http://graph.facebook.com/',
@@ -17,4 +20,20 @@ appRouter.get("/fb/pivotal", function(req, res) {
 	res.setBody(result);
 });
 
-module.exports = _router;
+router.post("/token", function(req, res){
+	var rate = new Rate();
+	var token = Math.floor((1 + Math.random()) * 0x1000000000000000).toString(24);
+	rate.setKey(token);
+	rate.setValue( req.body.value || req.parameters['value'] || 10 );
+	rate.setWindow( req.body.window || req.parameters['window'] || 120 );
+
+	rateLimiter.setRate(rate);
+
+	res.setBody({ 
+		'access_token': token,
+		'value': rate.getValue(),
+		'window': rate.getWindow()
+	});
+});
+
+module.exports = router;
